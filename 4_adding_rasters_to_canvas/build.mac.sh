629345f8144d0a15c40baf0a5a52e957c5222f02
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-03-11 03:35:09 +0000

    Copy srs.db and other qgis resources into the mac bundle
    git-svn-id: http://svn.osgeo.org/qgis/trunk@6792 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- 4_adding_rasters_to_canvas/build.mac.sh
+++ 4_adding_rasters_to_canvas/build.mac.sh
@@ -5,3 +5,5 @@ mkdir qgis_example4.app/Contents/MacOS/lib/
 cp /Applications/qgis.app/Contents/MacOS/lib/libqgis_core.dylib qgis_example4.app/Contents/MacOS/lib/
 cp /Applications/qgis.app/Contents/MacOS/lib/libqgis_gui.dylib qgis_example4.app/Contents/MacOS/lib/
 cp -r data qgis_example4.app/Contents/MacOS/
+mkdir -p qgis_example4.app/Contents/MacOS/share/qgis/resources/
+cp /Applications/qgis.app/Contents/MacOS/share/qgis/resources/* qgis_example4.app/Contents/MacOS/share/qgis/resources/
