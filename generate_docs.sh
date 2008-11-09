150739122a2224cf636cff99725c7a393b4edb9d
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-11-09 09:09:25 +0000

    Start migrating tutorials to text2tags format
    git-svn-id: http://svn.osgeo.org/qgis/trunk@9607 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ generate_docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# Simple bash script to generate tutorial documentation
+#
+
+txt2tags -t html -o index.html index.t2t
+txt2tags -t html -o tutorial1.html 1_hello_world_qgis_style/tutorial1.t2t
+
