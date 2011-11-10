f9ccdd35371d6e4c1a11553ba9b17defaf93f941
Tim Sutton <tim@linfiniti.com>
2011-11-11 00:52:10 +0200

    Added README from txt2tags

--- generate_docs.sh
+++ generate_docs.sh
@@ -7,5 +7,6 @@
 txt2tags -t html -o index.html index.t2t
 txt2tags -t html -o tutorial1.html 1_hello_world_qgis_style/tutorial1.t2t
 txt2tags -t html -o tutorial2.html 2_basic_main_window/tutorial2.t2t
-txt2tags -t html -o tutorial3.html 3_basic_labelling/tutorial3.t2
+txt2tags -t html -o tutorial3.html 3_basic_labelling/tutorial3.t2t
+txt2tags -t txt -o README index.t2t
 
